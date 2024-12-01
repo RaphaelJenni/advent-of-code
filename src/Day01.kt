d8b16d194190b13f3cfabaeba891f66afe8a7621
Raphael Jenni <info@rjenni.ch>
2024-12-01 18:10:12 +0100

    feat(Day-01): Initial, very quick and dirty solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
+import kotlin.math.abs
+
+const val SEPARATOR = "   "
+
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val testInput = readInput("data/day01/sample.day01")
+    val testDay01 = Day01(testInput)
+    check(testDay01.findDistance() == 11)
+    check(testDay01.similarityScore() == 31)
+
+    val input = readInput("data/day01/actual.day01")
+    val actualDay01 = Day01(input)
+    actualDay01.findDistance().println()
+    actualDay01.similarityScore().println()
+}
+
+
+class Day01(
+    input: List<String>
+) {
+    private val left = mutableListOf<Int>()
+    private val right = mutableListOf<Int>()
+
+    init {
+        input.forEach {
+            val value = it.split(SEPARATOR)
+            left.add(value[0].toInt())
+            right.add(value[1].toInt())
+        }
+
+        left.sort()
+        right.sort()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun findDistance(): Int {
+        var distance = 0
+        for (i in left.indices) {
+            distance += abs(right[i] - left[i])
+        }
+        return distance
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    fun similarityScore() : Int {
+        var score = 0
+        for (i in left.indices) {
+            val leftValue = left[i]
+            val rightValue = right.count { it == leftValue }
+            score += leftValue * rightValue
+        }
+        return score
+    }
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,10 +3,7 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("src/$name").readLines()
 
 /**
  * Converts string to md5 hash.
