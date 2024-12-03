09b3036e8366c7f6fc3b7e2b48cb2ca6fd0ff1b9
Raphael Jenni (gg526) <raphael.jenni@zkb.ch>
2024-12-03 09:18:41 +0100

    feat(Day-02): Add brute force approach

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,7 +4,7 @@ fun main() {
     val testInput = readInput("data/day02/sample.day02")
     val testDay02 = Day02(testInput)
     check(testDay02.numberOfSafeReports() == 2) { "Found ${testDay02.numberOfSafeReports()}" }
-    check(testDay02.numberOfSafeReportsWithTolerance() == 4)
+    check(testDay02.numberOfSafeReportsWithTolerance() == 5) { "Found ${testDay02.numberOfSafeReportsWithTolerance()}" }
 
     val input = readInput("data/day02/actual.day02")
     val actualDay02 = Day02(input)
@@ -38,49 +38,24 @@ private class Report(
     val levels: List<Int>,
 ) {
     fun isSafe(): Boolean {
-        if (levels.size <= 1) return true
-
-        if (levels[0] == levels[1]) return false
-
-        val isDescending = levels[0] > levels[1]
-
-        for (i in 1 until levels.size) {
-            val delta = abs(levels[i] - levels[i - 1])
-            if(delta !in 1..3) return false
-            if (isDescending) {
-                if (levels[i] > levels[i - 1]) return false
-            } else {
-                if (levels[i] < levels[i - 1]) return false
-            }
-        }
-        return true
+        return levels.isMonotonicAndRespectsLimit()
     }
 
     fun isSafeWithErrorTolerance(): Boolean {
-        if (levels.size <= 1) return true
+        if (isSafe()) return true
 
-        if (levels[0] == levels[1]) return false
-
-        val isDescending = levels[0] > levels[1]
-
-        var errors = 0
-
-        val correctedLevels = mutableListOf(levels[0])
-
-        for (i in 1 until levels.size) {
-            val last = correctedLevels.last()
-            val delta = abs(levels[i] - last)
-
-            val isError = (delta !in 1..3) || if (isDescending) levels[i] > last else levels[i] < last
-
-            if (isError) {
-                errors++
-            } else {
-                correctedLevels.add(levels[i])
-            }
-
-            if (errors > 1) return false
+        // brute force approach
+        return levels.indices.any { index ->
+            // remove one element at the time
+            levels.filterIndexed { i, _ -> i != index }.isMonotonicAndRespectsLimit()
         }
-        return true
+    }
+
+
+    private fun List<Int>.isMonotonicAndRespectsLimit(): Boolean {
+        val levelPairs = windowed(size = 2, step = 1, partialWindows = false)
+        val checkLimitRespect = { a: Int, b: Int -> abs(a - b) in 1..3 }
+        return levelPairs.all { (a, b) -> a > b && checkLimitRespect(a, b) } ||
+            levelPairs.all { (a, b) -> a < b && checkLimitRespect(a, b) }
     }
 }
